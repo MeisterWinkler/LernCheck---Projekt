0346660922f16c752ad38d908667f45760b2736e
MomchilsDaddy <timowausaanderg@gmail.com>
2026-01-30 10:09:53 +0100

    test änderungen fetch

--- backend/src/main/java/api/TestStudentController.java
+++ backend/src/main/java/api/TestStudentController.java
@@ -1,8 +1,14 @@
 package api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TestStudentController {
@@ -14,5 +20,12 @@ public class TestStudentController {
     @RequestMapping(path = "/student")
     public String index(){
         return "student-join";
+    }
+
+
+    @GetMapping("test/String")
+    public ResponseEntity<List<String>> testString(){
+       List<String> response = Arrays.asList("Test","Test2");
+       return new ResponseEntity<List<String>>(response, HttpStatus.OK);
     }
 }
